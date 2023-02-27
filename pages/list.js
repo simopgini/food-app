17a4b01e3086d092c052932099f7af05699c1172
Simona <simop.914@gmail.com>
2023-02-27 16:57:49 +0100

    routing

--- pages/list.js
+++ pages/list.js
@@ -10,11 +10,11 @@ export default function List() {
         <>
             <div className="xs:h-screen mb-8">
                 <div>
-                <div className='px-4 mb-6 mt-16 flex justify-between items-center'>
-                    <Link href="/" className='hover:scale-105 bg-white rounded-lg p-1 shadow-md'><ChevronLeftIcon className='hover:animate-pulse h-6 w-6'/></Link>
-                    <div className='flex-1 bg-white'></div>
-                    <div className='flex-auto p-1 font-bold text-xl'>LIST</div>
-                </div>
+                    <div className='px-4 mb-6 mt-16 flex justify-between items-center'>
+                        <Link href="/" className='hover:scale-105 bg-white rounded-lg p-1 shadow-md'><ChevronLeftIcon className='hover:animate-pulse h-6 w-6'/></Link>
+                        <div className='flex-1 bg-white'></div>
+                        <div className='flex-auto p-1 font-bold text-xl'>LIST</div>
+                    </div>
                 </div>
             
                 <div className='px-4 mb-8'>
--- pages/profile.js
+++ pages/profile.js
@@ -1,9 +1,19 @@
+import Link from 'next/link'
+import {  ChevronLeftIcon } from '@heroicons/react/24/solid'
 
 
 export default function Profile() {
     return (
         <>
-            
+            <div className="xs:h-screen mb-8">
+                <div>
+                    <div className='px-4 mb-6 mt-16 flex justify-between items-center'>
+                        <Link href="/" className='hover:scale-105 bg-white rounded-lg p-1 shadow-md'><ChevronLeftIcon className='hover:animate-pulse h-6 w-6'/></Link>
+                        <div className='flex-1 bg-white'></div>
+                        <div className='flex-auto p-1 font-bold text-xl'>Profile</div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
