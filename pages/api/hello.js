8634e9d3ca7424cb34f5c6e42d727a35004c3994
Simona <simop.914@gmail.com>
2023-02-27 20:01:39 +0100

    changes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,3 +3,18 @@
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
+
+
+
+// To handle different HTTP methods in an API route, you can use req.method 
+// in your request handler, like so:
+
+// export default function handler(req, res) {
+//   if (req.method === 'POST') {
+//     // Process a POST request
+//   } else {
+//     // Handle any other HTTP method
+//   }
+// }
+// To fetch API endpoints, take a look into any of the examples at the start of 
+// this section.
