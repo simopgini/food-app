01ee5e76c394a21f524806d842f10d0a3078f84f
Simona <simop.914@gmail.com>
2023-03-05 16:13:49 +0100

    px-2 added

--- pages/recipe.js
+++ pages/recipe.js
@@ -128,19 +128,19 @@ const recipe = ()  => {
                 </div>
 
                 <div className='mb-4 mx-4 flex items-center text-sm font-medium'>
-                      <div className='bg-green text-white rounded-lg h-6 w-6 mr-2 flex items-center justify-center'><span>1</span></div>
+                      <div className='bg-green text-white rounded-lg h-6 w-6 mr-2 flex items-center justify-center'><span className='px-2'>1</span></div>
                       <p>Mash your avocado separately.</p>
                 </div>
                 <div className='mb-4 mx-4 flex items-center text-sm font-medium'>
-                      <div className='bg-green text-white rounded-lg h-6 w-6 mr-2 flex items-center justify-center'><span>2</span></div>
+                      <div className='bg-green text-white rounded-lg h-6 w-6 mr-2 flex items-center justify-center'><span className='px-2'>2</span></div>
                       <p>Slice cherry tomatoes and add salt and pepper.</p>
                 </div>
                 <div className='mb-4 mx-4 flex items-center text-sm font-medium'>
-                      <div className='bg-green text-white rounded-lg h-6 w-6 mr-2 flex items-center justify-center'><span>3</span></div>
+                      <div className='bg-green text-white rounded-lg h-6 w-6 mr-2 flex items-center justify-center'><span className='px-2'>3</span></div>
                       <p>Buy good bread and toast it well. It offers a sturdy base and a crisp contrast against the creamy avocado.</p>
                 </div>
                 <div className='mb-4 mx-4 flex items-center text-sm font-medium'>
-                      <div className='bg-green text-white rounded-lg h-6 w-6 mr-2 flex items-center justify-center'><span>4</span></div>
+                      <div className='bg-green text-white rounded-lg h-6 w-6 mr-2 flex items-center justify-center'><span className='px-2'>4</span></div>
                       <p>Don’t forget lime and paprika. </p>
                 </div>
               </section>
