d797be1320534a6ca3572651588b0dfab47cc2c3
Simona <simop.914@gmail.com>
2023-03-11 17:36:02 +0100

    horizcard image config

--- components/HorizontalCardHome.js
+++ components/HorizontalCardHome.js
@@ -12,18 +12,21 @@ return (
     <div className="my-4 flex-none w-2/3 md:w-1/3 mr-8 md:pb-4 bg-green rounded-lg">
         <Link href="/recipe" className="space-y-2">
             {/* se funziona Image aggiungi: relative, h e w perchè l'img riempirà il container */}
-            <div className="aspect-w-16 aspect-h-9">
-                <Image 
-                    priority
-                    src={recipe.image} 
-                    alt={recipe.title} fill
-                    objectfit="contain"/>
-                {/* <img 
+            {/* <div className="aspect-w-16 aspect-h-9">
+                <img 
                     className="contrast-[1.1] saturate-[1.4] object-cover shadow-md hover:shadow-xl h-20 md:h-24 xl:h-40 rounded-br-xl rounded-tl-lg"
                     src={recipe.image} 
                     alt={recipe.title} 
-                    /> */}
-                    {/* // fill=true */}
+                    />
+            </div> */}
+            <div className='relative w-full h-60'>
+                <Image 
+                    priority
+                    src={recipe.image} 
+                    alt={recipe.title} 
+                    fill
+                    style={{objectFit:"contain"}}
+                />
             </div>
             <div className="px-2 py-2 text-white">
                 <h2 className="text-base pb-1 font-semibold truncate">{recipe.title}</h2>
--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
  const nextConfig = {
    reactStrictMode: true,
-   images: {
-    domains: ['spoonacular.com'],
-  },
  }
+
+ module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'spoonacular.com',
+        port: '',
+        pathname: '/recipeImages/**',
+      },
+    ],
+  },
+}
 
 // module.exports = nextConfig;
 
