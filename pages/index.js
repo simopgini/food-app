fe5958842454ce2d223be1f2e17ee60b2ffba2c1
Simona <simop.914@gmail.com>
2023-03-11 23:21:46 +0100

    refactoring

--- pages/index.js
+++ pages/index.js
@@ -176,6 +176,25 @@ export default function Home() {
               </div>
             </section>
 
+            <section className=''>
+              <div className='px-4 pb-3 flex justify-between items-center font-bold'>
+                <h1 className='text-dark-green text-lg font-bold'>Popular Recipes</h1>
+                <div className='text-green text-xs'>See all</div>
+              </div>
+
+              <div className='items-stretch grid 2xl:grid-cols-7 xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 md:gap-x-10 md:gap-y-10 sm:gap-x-8 sm:gap-y-8 gap-x-2 gap-y-2 text-white px-4'>
+                {recipes.results.map((recipe) => {
+                return (
+                  <VerticalCard key={recipe.id} recipe={recipe} className=" "/>
+                    )
+                  })
+                }
+              </div>
+            </section>
+          
+          </div>
+      </div>
+
             {/* <section className='mb-3'>
               <div className='px-4 flex justify-between items-center font-bold'>
                 <h1 className='text-dark-green text-lg font-bold'>Category</h1>
@@ -497,7 +516,7 @@ export default function Home() {
               </div>
             </section> */}
 
-            <section className=''>
+            {/* <section className=''>
               <div className='px-4 pb-3 flex justify-between items-center font-bold'>
                 <h1 className='text-dark-green text-lg font-bold'>Popular Recipes</h1>
                 <div className='text-green text-xs'>See all</div>
@@ -514,7 +533,8 @@ export default function Home() {
             </section>
           </div>
         
-        </div>
+        </div> */}
+
 
 
 
