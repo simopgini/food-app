c370f08cdac6583f0854a1721f78a8af52693074
Simona <simop.914@gmail.com>
2023-03-16 11:22:05 +0100

    switcher ready

--- components/Switcher.js
+++ components/Switcher.js
@@ -1,21 +1,33 @@
 import { useState, useEffect } from "react";
-import classNames from "classnames"
+import classNames from "classnames";
 
 const Switcher = () => {
-    const [isSelected, setIsSelected] = useState(false)
-
+  const [isSelected, setIsSelected] = useState(true);
 
   return (
-    <button onClick={()=>{setIsSelected(!isSelected)}}
-            className={classNames(" hover:shadow-lg  outline-1 flex items-center pl-1 w-full h-12 bg-gray-lighter rounded-2xl font-medium", 
-            (isSelected ? 'bg-dark-green' : 'bg-gray-lighter')
-            )}>
-            <span onClick={()=>{setIsSelected(!isSelected)}}
-            className={classNames(" active:shadow-dark-green-500/50 hover:shadow-dark-green-500/50 transition-400 hover:shadow-xl  h-10 w-1/2 bg-dark-green rounded-xl mr-2 font-medium text-white flex items-center justify-center", 
-            (isSelected ? 'bg-gray-lighter' : 'bg-dark-green')
-            )}>Ingredients</span>  
-            Instructions
-    </button> 
+    <button
+      onClick={() => {
+        setIsSelected(!isSelected);
+      }}
+      className={classNames(
+        " hover:shadow-lg  outline-1 flex items-center pl-1 w-full h-12 rounded-2xl font-medium ",
+        isSelected ? `bg-gray-mid text-dark-green` : `bg-dark-green text-white`
+      )}
+    >
+                {/* "bg-red-400" : "bg-dark-green" */}
+      <span
+        // onClick={() => {
+        //   setIsSelected(isSelected);
+        // }}
+        className={classNames(
+          " active:shadow-dark-green-500/50 hover:shadow-dark-green-500/50 transition-400 hover:shadow-xl  h-10 w-1/2 rounded-xl mr-2 font-medium flex items-center justify-center",
+          isSelected ? `bg-dark-green text-white` : `bg-gray-mid text-dark-green`
+        )}
+      >
+        Ingredients
+      </span>
+      Instructions
+    </button>
     // <button onClick={()=>{setIsSelected(!isSelected)}}
     //         className={classNames("flex items-center pl-1 w-full h-12 bg-gray-lighter rounded-2xl font-medium", {
     //         'bg-dark-green': isSelected,
@@ -23,9 +35,9 @@ const Switcher = () => {
     //     <span className={classNames("h-10 w-1/2 bg-dark-green rounded-xl mr-2 font-medium text-white flex items-center justify-center", {
     //         'bg-gray-lighter': !isSelected,
     //     }
-    //     )}>Ingredients</span>  
+    //     )}>Ingredients</span>
     //     Instructions
-    // </button> 
+    // </button>
   );
 };
 
--- pages/[recipeId]/index.js
+++ pages/[recipeId]/index.js
@@ -15,27 +15,28 @@ import Switcher from "@/components/Switcher";
 import Instructions from "@/components/Instructions";
 // import recipeJson from "../components/recipeJson";
 
-
-
-const Recipe = ({recipe}) => {
-  
-  const [recipeCard, setRecipeCard] = useState()
-  const  fetchRecipe = async () => { 
+const Recipe = ({ recipe }) => {
+  const [recipeCard, setRecipeCard] = useState();
+  const fetchRecipe = async () => {
     // const r = await fetch("https://api.spoonacular.com/recipes/716429/information?apiKey=5ea4af906f4443dba9c723a359aa6533&includeNutrition=true")
     // const r = await fetch(`https://api.spoonacular.com/recipes/${recipe.id}/information?apiKey=5ea4af906f4443dba9c723a359aa6533&includeNutrition=true`)
 
     // const data =await r.text();
-  const data = JSON.parse(recipeJson)
+    const data = JSON.parse(recipeJson);
     // console.log(recipeJson.extendedIngredients.name)
     setRecipeCard(data);
-  }
-  
-  useEffect(() => {
-    fetchRecipe()
-  }, [])
+  };
 
-  if(recipeCard === undefined){
-    return <div className="flex justify-center items-center text-base text-dark-green">Loading...</div>
+  useEffect(() => {
+    fetchRecipe();
+  }, []);
+
+  if (recipeCard === undefined) {
+    return (
+      <div className="flex justify-center items-center text-base text-dark-green">
+        Loading...
+      </div>
+    );
   }
   // console.log(recipeCard.extendedIngredients.name)
   // console.log(recipeJson)
@@ -43,8 +44,7 @@ const Recipe = ({recipe}) => {
     <>
       {/* c'è un import chiamato classNames che permette di prendere più classNames, prova per vedere 
           se si riesce a mettere l'img come sfondo in maniera dinamica */}
-      <div
-        className="immagine bg-no-repeat hover:transition hover:duration-300 hover:object-none hover:object-center contrast-[1.1] saturate-[1.4]">
+      <div className="immagine bg-no-repeat hover:transition hover:duration-300 hover:object-none hover:object-center contrast-[1.1] saturate-[1.4]">
         {/* h-96  */}
         <div className="px-4 pt-16 flex justify-between items-center mb-24">
           <Link href="/" className="bg-gray-mid rounded-lg p-1">
@@ -65,10 +65,11 @@ const Recipe = ({recipe}) => {
               {/* })} */}
               <p className="flex text-gray">
                 <HiOutlineClock className="h-4 w-4 mr-1" />
-                <span className="font-light text-xs">{recipeCard.readyInMinutes} min</span>
+                <span className="font-light text-xs">
+                  {recipeCard.readyInMinutes} min
+                </span>
               </p>
             </div>
-
 
             {/* <div>{recipeCard.nutrition.nutrients[0].amount}prova</div>
 
@@ -78,7 +79,6 @@ const Recipe = ({recipe}) => {
 
             <div className="flex justify-between text-dark-green">
               <div className="text-sm font-semibold">
-
                 <div className="mb-4 flex items-center">
                   <div className="bg-gray-mid rounded-lg p-1 mr-2">
                     <Image
@@ -99,7 +99,6 @@ const Recipe = ({recipe}) => {
                 </div>
               </div>
               <div className="mb-8 text-sm font-semibold">
-                
                 <div className="mb-4 flex items-center">
                   <div className="bg-gray-mid rounded-lg p-1 mr-2">
                     <Image
@@ -124,17 +123,19 @@ const Recipe = ({recipe}) => {
                 </div>
               </div>
             </div>
-
+          </section>
+            
+            <div className="px-4 pb-4 ">
               <Switcher />
+            </div>
 
-            <div className="bg-gray-mid py-1.5 px-1 flex rounded-xl font-medium">
+            {/* <div className="bg-gray-mid py-1.5 px-1 flex rounded-xl font-medium">
               <button className="text-gray-mid bg-dark-green px-10 py-2 rounded-xl">
                 Ingredients
               </button>
               <button className="text-dark-green pl-10">Instructions</button>
-            </div>
-
-          </section>
+            </div> */}
+            
 
           {/* INGREDIENTS SECTION */}
           <section className="px-4">
@@ -143,59 +144,58 @@ const Recipe = ({recipe}) => {
                 Ingredients
               </h1>
               <p className="flex text-gray">
-                <span className="font-light text-xs">Items</span>
+                <span className="font-light text-xs">Quantity</span>
               </p>
             </div>
 
             <div>
               {recipeCard.extendedIngredients.map((ingredient) => {
-                        return (
-                          <Ingredients
-                            key={ingredient.id}
-                            ingredient={ingredient}
-                            quantity={ingredient.original}
-                            amount={ingredient.amount}
-                            unit={ingredient.unit}
-                            className=" "
-                          />
+                return (
+                  <Ingredients
+                    key={ingredient.id}
+                    ingredient={ingredient}
+                    quantity={ingredient.original}
+                    amount={ingredient.amount}
+                    unit={ingredient.unit}
+                    className=" "
+                  />
                 );
               })}
             </div>
           </section>
 
-            <section>
-              <div className="px-4 flex justify-between items-baseline">
-                <h1 className=" mb-6 text-dark-green text-xl font-bold">
-                  Instructions
-                </h1>
-                <p className="flex text-gray">
-                  <span className="font-light text-xs">4 steps</span>
-                </p>
-              </div>
-              <div>
-                {recipeCard.extendedIngredients.map((instruction, index) => {
-                        return (
-                          <Instructions
-                            key={instruction.id}
-                            ingredient={instruction}
-                            quantity={instruction.original}
-                            index={index}
-                            className=" "
-                          />
-                  );
-                })}
-              </div>
-            </section>
-
+          <section>
+            <div className="px-4 flex justify-between items-baseline">
+              <h1 className=" mb-6 text-dark-green text-xl font-bold">
+                Instructions
+              </h1>
+              <p className="flex text-gray">
+                <span className="font-light text-xs">Steps</span>
+              </p>
+            </div>
+            <div>
+              {recipeCard.extendedIngredients.map((instruction, index = 0) => {
+                return (
+                  <Instructions
+                    key={instruction.id}
+                    instruction={instruction}
+                    index={index}
+                    className=" "
+                  />
+                );
+              })}
+            </div>
+          </section>
           {/* INSTRUCTIONS SECTION */}
           <section>
-
-
             <div className="mb-4 mx-4 flex items-center text-sm font-medium">
               <div className="bg-green text-white rounded-lg h-6 w-6 mr-2 flex items-center justify-center">
                 <span className="px-2">1</span>
               </div>
-              <p>Mash your avocado separately.</p>
+              <p>
+                {recipeCard.extendedIngredients[0].original} Mash your avocado
+                separately.
+              </p>
             </div>
             <div className="mb-4 mx-4 flex items-center text-sm font-medium">
               <div className="bg-green text-white rounded-lg h-6 w-6 mr-2 flex items-center justify-center">
